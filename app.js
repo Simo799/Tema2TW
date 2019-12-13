0c7623b73542396d6af5506370b267818af89961
EC2 Default User <ec2-user@ip-172-31-41-108.us-east-2.compute.internal>
2019-12-13 16:28:12 +0000

    Tema2

---
+++ app.js
@@ -0,0 +1,40 @@
+function addTokens(input, tokens){
+    
+    if(typeof input!=="string"){
+        throw ("Invalid input");
+    }
+    if(input.length<6){
+        throw ("Input should have at least 6 characters");
+
+    }
+
+    
+
+    for(i=0;i<tokens.length;i++){
+
+        if(typeof tokens[i].tokenName!=="string"){
+            throw ("Invalid array format");
+        }
+    }
+    
+    
+    if(input.includes("..."))
+    {
+        for(i=0;i<tokens.length;i++){
+            input = input.replace("...", "${"+tokens[i].tokenName+"}");
+            return input;
+        }
+    }
+    else return input;
+    
+}
+
+
+
+const app = {
+    addTokens: addTokens
+}
+
+module.exports = app;
+
+
